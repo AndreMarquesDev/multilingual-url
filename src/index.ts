8bb2f3d6653ce4b48d7da28c8d21e45de21971a8
André Marques <andregm_7@hotmail.com>
2020-05-05 23:35:00 +0100

    Stop exporting urlHasLocale in favor of getLangFromUrl

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export type { IPrismicCultures } from './typings/index';
 export { isClientSide } from './generic';
 export { getInitialLocale, getPrismicLocale } from './getLocale';
-export { getRemainingLang, urlHasLocale } from './urlValidation';
+export { getLangFromUrl, getRemainingLang } from './urlValidation';
